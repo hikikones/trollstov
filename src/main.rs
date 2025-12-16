8f8543ae5cb22022f031cd3be8f98a99c3925458
hikikones <hikikones@gmail.com>
2025-12-16 21:18:17 +0100

    Traverse music files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let dir = std::env::args().last().expect("expected dir path");
+
+    for entry in walkdir::WalkDir::new(dir)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|entry| entry.file_type().is_file())
+        .filter(|entry| {
+            entry
+                .file_name()
+                .to_str()
+                .map(|s| s.ends_with(".opus"))
+                .unwrap_or(false)
+        })
+    {
+        println!("{}", entry.path().display());
+    }
 }
