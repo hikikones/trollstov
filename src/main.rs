d05a1a2a27e4b12cb403d63921c36ca50e735173
hikikones <hikikones@gmail.com>
2025-12-18 01:27:41 +0100

    Make traversal a function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 fn main() {
     let dir = std::env::args().last().expect("expected dir path");
 
-    for entry in walkdir::WalkDir::new(dir)
+    for path in traverse_audio_files(dir) {
+        println!("{}", path.display());
+    }
+}
+
+fn traverse_audio_files(
+    root: impl AsRef<std::path::Path>,
+) -> impl Iterator<Item = std::path::PathBuf> {
+    const AUDIO_EXTS: &[&str] = &[
+        "mp3", "wav", "flac", "ogg", "opus", "aac", "m4a", "wma", "alac", "aiff",
+    ];
+
+    walkdir::WalkDir::new(root)
         .follow_links(true)
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|entry| entry.file_type().is_file())
-        .filter(|entry| {
-            entry
-                .file_name()
-                .to_str()
-                .map(|s| s.ends_with(".opus"))
+        .map(|file| file.into_path())
+        .filter(|path| {
+            path.extension()
+                .map(|file_ext| {
+                    AUDIO_EXTS
+                        .iter()
+                        .any(|audio_ext| file_ext.eq_ignore_ascii_case(audio_ext))
+                })
                 .unwrap_or(false)
         })
-    {
-        println!("{}", entry.path().display());
-    }
 }
