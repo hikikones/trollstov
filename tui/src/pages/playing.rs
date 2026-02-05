eefdaade80d0e71da0e7ae5f268080d09170d351
hikikones <hikikones@gmail.com>
2026-02-05 23:15:29 +0100

    Remove `AppEvent::UpdateAndRender` and render on update instead (#33)

--- tui/src/pages/playing.rs
+++ tui/src/pages/playing.rs
@@ -43,48 +43,23 @@ impl PlayingPage {
 
     pub fn on_enter(&self) {}
 
-    pub fn on_update(&mut self, jb: &Jukebox) {
+    pub fn on_update(&mut self, jb: &Jukebox) -> bool {
+        let mut render = false;
+
         if self.current != jb.current_track_id() {
             self.current = jb.current_track_id();
-
-            // Track has changed, time to update image
-            match jb.current_track_id() {
-                Some(tid) => {
-                    // Load image in thread and store handle
-                    self.image = FrontCover::Loading;
-                    let path = jb.get(tid).unwrap().path().to_path_buf();
-                    let picker = self.picker.clone();
-                    let handle = std::thread::spawn(move || {
-                        let picture = AudioPicture::read(path).unwrap();
-                        match picture.bytes() {
-                            Some(bytes) => {
-                                const MAX_RES: u32 = 720;
-                                let mut dyn_img = image::load_from_memory(bytes).unwrap();
-                                let (w, h) = dyn_img.dimensions();
-                                if w > MAX_RES || h > MAX_RES {
-                                    dyn_img = dyn_img.thumbnail(MAX_RES, MAX_RES);
-                                }
-                                FrontCover::Ready(picker.new_resize_protocol(dyn_img))
-                            }
-                            None => FrontCover::None,
-                        }
-                    });
-                    self.image_handle = Some(handle);
-                }
-                None => {
-                    // No track currently playing, remove image
-                    self.image = FrontCover::None;
-                    self.image_handle = None;
-                }
-            }
-        } else if let Some(handle) = self.image_handle.take() {
+            self.update_image(jb);
+            render = true;
+        } else if let Some(handle) = self.image_handle.as_ref() {
             // Poll thread for finished
             if handle.is_finished() {
+                let handle = self.image_handle.take().unwrap();
                 self.image = handle.join().unwrap();
-            } else {
-                self.image_handle = Some(handle);
+                render = true;
             }
         }
+
+        render
     }
 
     pub fn on_render(&mut self, area: Rect, buf: &mut Buffer, jb: &Jukebox, colors: &Colors) {
@@ -269,6 +244,39 @@ impl PlayingPage {
                 );
             },
         );
+    }
+
+    fn update_image(&mut self, jb: &Jukebox) {
+        // Track has changed, time to update image
+        match jb.current_track_id() {
+            Some(tid) => {
+                // Load image in thread and store handle
+                self.image = FrontCover::Loading;
+                let path = jb.get(tid).unwrap().path().to_path_buf();
+                let picker = self.picker.clone();
+                let handle = std::thread::spawn(move || {
+                    let picture = AudioPicture::read(path).unwrap();
+                    match picture.bytes() {
+                        Some(bytes) => {
+                            const MAX_RES: u32 = 720;
+                            let mut dyn_img = image::load_from_memory(bytes).unwrap();
+                            let (w, h) = dyn_img.dimensions();
+                            if w > MAX_RES || h > MAX_RES {
+                                dyn_img = dyn_img.thumbnail(MAX_RES, MAX_RES);
+                            }
+                            FrontCover::Ready(picker.new_resize_protocol(dyn_img))
+                        }
+                        None => FrontCover::None,
+                    }
+                });
+                self.image_handle = Some(handle);
+            }
+            None => {
+                // No track currently playing, remove image
+                self.image = FrontCover::None;
+                self.image_handle = None;
+            }
+        }
     }
 }
 
