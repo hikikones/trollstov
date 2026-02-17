ee846f9438bf8ae40fa44e8e98c8db2f9be8035f
hikikones <hikikones@gmail.com>
2026-02-17 17:27:16 +0100

    use points callback instead

--- tui/src/widgets/logo.rs
+++ tui/src/widgets/logo.rs
@@ -4,33 +4,38 @@ pub struct LogoWidget;
 
 impl LogoWidget {
     pub fn render(area: Rect, buf: &mut Buffer) {
-        let (cx, cy) = (area.width / 2, area.height / 2);
+        let center = (area.width / 2, area.height / 2);
         let radius = 10;
         let color = Color::Yellow;
 
-        draw_circle(buf, cx, cy, radius, color, true);
+        circle_points(center, radius, |p| {
+            if p.1 < center.1
+                && let Some(cell) = buf.cell_mut(p)
+            {
+                cell.set_bg(color);
+            }
+        });
 
-        let ray_count = 12;
+        let ray_count = 14;
         let ray_length = radius / 2;
         let ray_offset = radius / 4;
 
-        for i in 0..ray_count {
-            let angle = i as f32 * (2.0 * std::f32::consts::PI / ray_count as f32);
-
-            let inner_r = (radius + ray_offset) as f32;
-            let outer_r = (radius + ray_offset + ray_length) as f32;
-
-            let x1 = cx as f32 + inner_r * angle.cos() * 2.0;
-            let y1 = cy as f32 + inner_r * angle.sin();
-
-            let x2 = cx as f32 + outer_r * angle.cos() * 2.0;
-            let y2 = cy as f32 + outer_r * angle.sin();
-
-            let p1 = (x1.round() as u16, y1.round() as u16);
-            let p2 = (x2.round() as u16, y2.round() as u16);
-
-            draw_line(p1, p2, color, buf);
-        }
+        ray_lines(
+            center,
+            radius,
+            ray_count,
+            ray_length,
+            ray_offset,
+            |p1, p2| {
+                if p2.1 < center.1 {
+                    line_points(p1, p2, |p| {
+                        if let Some(cell) = buf.cell_mut(p) {
+                            cell.set_bg(color);
+                        }
+                    });
+                }
+            },
+        );
 
         super::utils::print_ascii(
             area,
@@ -42,14 +47,13 @@ impl LogoWidget {
     }
 }
 
-fn draw_circle(buf: &mut Buffer, cx: u16, cy: u16, radius: u16, color: Color, top_half_only: bool) {
+fn circle_points(center: (u16, u16), radius: u16, mut f: impl FnMut((u16, u16))) {
+    let (cx, cy) = (center.0 as i16, center.1 as i16);
+    let x_scale = 2;
+
     let mut x = radius as i16;
     let mut y = 0;
     let mut err = 0;
-
-    let cx = cx as i16;
-    let cy = cy as i16;
-    let x_scale = 2;
 
     while x >= y {
         let points = [
@@ -73,13 +77,7 @@ fn draw_circle(buf: &mut Buffer, cx: u16, cy: u16, radius: u16, color: Color, to
 
         for (px, py) in points {
             if px >= 0 && py >= 0 {
-                if top_half_only && py > cy {
-                    continue;
-                }
-
-                if let Some(cell) = buf.cell_mut((px as u16, py as u16)) {
-                    cell.set_bg(color);
-                }
+                f((px as u16, py as u16));
             }
         }
 
@@ -94,7 +92,40 @@ fn draw_circle(buf: &mut Buffer, cx: u16, cy: u16, radius: u16, color: Color, to
     }
 }
 
-fn draw_line(p1: (u16, u16), p2: (u16, u16), color: Color, buf: &mut Buffer) {
+fn ray_lines(
+    center: (u16, u16),
+    circle_radius: u16,
+    ray_count: u8,
+    ray_length: u16,
+    ray_offset: u16,
+    mut f: impl FnMut((u16, u16), (u16, u16)),
+) {
+    let (cx, cy) = (center.0 as i16, center.1 as i16);
+    let radius = circle_radius as f32;
+    let length = ray_length as f32;
+    let offset = ray_offset as f32;
+    let x_scale = 2.0;
+
+    for i in 0..ray_count {
+        let angle = i as f32 * (2.0 * std::f32::consts::PI / ray_count as f32);
+
+        let inner_r = radius + offset;
+        let outer_r = radius + offset + length;
+
+        let x1 = cx as f32 + inner_r * angle.cos() * x_scale;
+        let y1 = cy as f32 + inner_r * angle.sin();
+
+        let x2 = cx as f32 + outer_r * angle.cos() * x_scale;
+        let y2 = cy as f32 + outer_r * angle.sin();
+
+        let p1 = (x1.round() as u16, y1.round() as u16);
+        let p2 = (x2.round() as u16, y2.round() as u16);
+
+        f(p1, p2)
+    }
+}
+
+fn line_points(p1: (u16, u16), p2: (u16, u16), mut f: impl FnMut((u16, u16))) {
     let (mut x1, mut y1) = (p1.0 as i16, p1.1 as i16);
     let (x2, y2) = (p2.0 as i16, p2.1 as i16);
 
@@ -105,9 +136,7 @@ fn draw_line(p1: (u16, u16), p2: (u16, u16), color: Color, buf: &mut Buffer) {
     let mut err = dx + dy;
 
     loop {
-        if let Some(cell) = buf.cell_mut((x1 as u16, y1 as u16)) {
-            cell.set_bg(color);
-        }
+        f((x1 as u16, y1 as u16));
 
         if x1 == x2 && y1 == y2 {
             break;
