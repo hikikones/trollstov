7d97afc132308da4c3071cce2665e7b9822b47be
hikikones <hikikones@gmail.com>
2026-03-01 13:00:43 +0100

    even better

--- tui/src/widgets/utils.rs
+++ tui/src/widgets/utils.rs
@@ -8,48 +8,7 @@ pub fn print_ascii(
     style: impl Into<Style>,
     alignment: Option<Alignment>,
 ) -> Rect {
-    let ascii = ascii.as_ref();
-    debug_assert!(ascii.is_ascii());
-
-    let (mut x, y, mut width) = match alignment {
-        Some(alignment) => {
-            let Rect { x, y, .. } = align(
-                Rect {
-                    width: line.width.min(ascii.len() as u16),
-                    height: 1,
-                    ..line
-                },
-                line,
-                alignment,
-            );
-            let width = (line.x + line.width).saturating_sub(x);
-            (x, y, width)
-        }
-        None => (line.x, line.y, line.width),
-    };
-
-    let style = style.into();
-    for ch in ascii.chars() {
-        if width == 0 {
-            break;
-        }
-
-        let Some(cell) = buf.cell_mut((x, y)) else {
-            break;
-        };
-
-        cell.set_char(ch).set_style(style);
-
-        x += 1;
-        width -= 1;
-    }
-
-    Rect {
-        x,
-        y,
-        width,
-        ..line
-    }
+    print_asciis_with_styles(line, buf, [(ascii.as_ref(), style.into())], alignment)
 }
 
 /// Prints ascii collection assuming only ASCII, no newlines or control characters.
@@ -60,50 +19,8 @@ pub fn print_asciis<'a>(
     style: impl Into<Style>,
     alignment: Option<Alignment>,
 ) -> Rect {
-    let asciis = asciis.into_iter();
-
-    let (mut x, y, mut width) = match alignment {
-        Some(alignment) => {
-            let ascii_width = asciis.clone().map(|s| s.len() as u16).sum::<u16>();
-            let Rect { x, y, .. } = align(
-                Rect {
-                    width: line.width.min(ascii_width),
-                    ..line
-                },
-                line,
-                alignment,
-            );
-            let width = (line.x + line.width).saturating_sub(x);
-            (x, y, width)
-        }
-        None => (line.x, line.y, line.width),
-    };
-
     let style = style.into();
-    'outer: for ascii in asciis {
-        debug_assert!(ascii.is_ascii());
-        for ch in ascii.chars() {
-            if width == 0 {
-                break 'outer;
-            }
-
-            let Some(cell) = buf.cell_mut((x, y)) else {
-                break 'outer;
-            };
-
-            cell.set_char(ch).set_style(style);
-
-            x += 1;
-            width -= 1;
-        }
-    }
-
-    Rect {
-        x,
-        y,
-        width,
-        ..line
-    }
+    print_asciis_with_styles(line, buf, asciis.into_iter().map(|s| (s, style)), alignment)
 }
 
 /// Prints ascii collection with styles assuming only ASCII, no newlines or control characters.
@@ -121,6 +38,7 @@ pub fn print_asciis_with_styles<'a>(
             let Rect { x, y, .. } = align(
                 Rect {
                     width: line.width.min(ascii_width),
+                    height: 1,
                     ..line
                 },
                 line,
@@ -158,81 +76,6 @@ pub fn print_asciis_with_styles<'a>(
     }
 }
 
-/// Prints char `n` times.
-pub fn print_char_repeat(
-    line: Rect,
-    buf: &mut Buffer,
-    ch: char,
-    n: u8,
-    style: impl Into<Style>,
-) -> Rect {
-    let style = style.into();
-    let char_width = unicode_width::UnicodeWidthChar::width(ch).unwrap_or(0) as u16;
-    let Rect {
-        mut x,
-        y,
-        mut width,
-        ..
-    } = line;
-
-    for _ in 0..n {
-        if width == 0 {
-            break;
-        }
-
-        match buf.cell_mut((x, y)) {
-            Some(cell) => {
-                cell.set_char(ch).set_style(style);
-            }
-            None => break,
-        }
-
-        width = width.saturating_sub(char_width);
-        x += char_width;
-    }
-
-    Rect {
-        x,
-        y,
-        width,
-        height: line.height,
-    }
-}
-
-/// Prints text `n` times.
-pub fn print_text_repeat(
-    line: Rect,
-    buf: &mut Buffer,
-    text: &str,
-    n: u8,
-    style: impl Into<Style>,
-) -> Rect {
-    let style = style.into();
-    let Rect {
-        mut x,
-        y,
-        mut width,
-        ..
-    } = line;
-
-    for _ in 0..n {
-        if width == 0 {
-            break;
-        }
-
-        let (next_x, _) = buf.set_stringn(x, y, text, width as usize, style);
-        width -= next_x - x;
-        x = next_x;
-    }
-
-    Rect {
-        x,
-        y,
-        width,
-        height: line.height,
-    }
-}
-
 /// Prints text and fills remaining empty cells with the given style.
 pub fn print_text(
     line: Rect,
@@ -242,59 +85,14 @@ pub fn print_text(
     fill: bool,
     alignment: Option<Alignment>,
 ) {
-    let text = text.as_ref();
     let style = style.into();
-
-    match alignment {
-        Some(alignment) => {
-            let text_width = unicode_width::UnicodeWidthStr::width(text);
-            let Rect { x, y, .. } = align(
-                Rect {
-                    width: line.width.min(text_width as u16),
-                    height: 1,
-                    ..line
-                },
-                line,
-                alignment,
-            );
-            let width = (line.x + line.width).saturating_sub(x);
-            let (end_x, _) = buf.set_stringn(x, y, text, width as usize, style);
-
-            if fill {
-                let pre = x - line.x;
-                for i in 0..pre {
-                    if let Some(cell) = buf.cell_mut((line.x + i, y)) {
-                        cell.set_style(style);
-                    }
-                }
-                let post = line.width - (end_x - x);
-                for i in 0..post {
-                    match buf.cell_mut((end_x + i, y)) {
-                        Some(cell) => {
-                            cell.set_style(style);
-                        }
-                        None => break,
-                    }
-                }
-            }
-        }
-        None => {
-            let Rect { x, y, width, .. } = line;
-            let (end_x, _) = buf.set_stringn(x, y, text, width as usize, style);
-
-            if fill {
-                let remaining = width - (end_x - x);
-                for i in 0..remaining {
-                    match buf.cell_mut((end_x + i, y)) {
-                        Some(cell) => {
-                            cell.set_style(style);
-                        }
-                        None => break,
-                    }
-                }
-            }
-        }
-    }
+    print_texts_with_styles(
+        line,
+        buf,
+        [(text.as_ref(), style)],
+        fill.then_some(style),
+        alignment,
+    );
 }
 
 /// Prints a collection of texts.
@@ -308,81 +106,13 @@ pub fn print_texts<'a>(
     alignment: Option<Alignment>,
 ) {
     let style = style.into();
-    let texts = texts.into_iter();
-
-    match alignment {
-        Some(alignment) => {
-            let text_width = texts
-                .clone()
-                .map(|s| unicode_width::UnicodeWidthStr::width(s))
-                .sum::<usize>();
-            let Rect { mut x, y, .. } = align(
-                Rect {
-                    width: line.width.min(text_width as u16),
-                    height: 1,
-                    ..line
-                },
-                line,
-                alignment,
-            );
-            let mut width = (line.x + line.width).saturating_sub(x);
-            let mut end_x = 0;
-            for text in texts {
-                if width == 0 {
-                    break;
-                }
-                (end_x, _) = buf.set_stringn(x, y, text, width as usize, style);
-                width -= end_x - x;
-                x = end_x;
-            }
-
-            if fill {
-                let pre = x - line.x;
-                for i in 0..pre {
-                    if let Some(cell) = buf.cell_mut((line.x + i, y)) {
-                        cell.set_style(style);
-                    }
-                }
-                let post = line.width - (end_x - x);
-                for i in 0..post {
-                    match buf.cell_mut((end_x + i, y)) {
-                        Some(cell) => {
-                            cell.set_style(style);
-                        }
-                        None => break,
-                    }
-                }
-            }
-        }
-        None => {
-            let Rect {
-                mut x,
-                y,
-                mut width,
-                ..
-            } = line;
-
-            for text in texts {
-                if width == 0 {
-                    break;
-                }
-                let (next_x, _) = buf.set_stringn(x, y, text, width as usize, style);
-                width -= next_x - x;
-                x = next_x;
-            }
-
-            if fill {
-                for i in 0..width {
-                    match buf.cell_mut((x + i, y)) {
-                        Some(cell) => {
-                            cell.set_style(style);
-                        }
-                        None => break,
-                    }
-                }
-            }
-        }
-    }
+    print_texts_with_styles(
+        line,
+        buf,
+        texts.into_iter().map(|s| (s, style)),
+        fill.then_some(style),
+        alignment,
+    );
 }
 
 /// Prints a collection of texts with styles.
@@ -513,6 +243,81 @@ pub fn print_text_segments_with_styles<'a>(
                 }
             }
         }
+    }
+}
+
+/// Prints char `n` times.
+pub fn print_char_repeat(
+    line: Rect,
+    buf: &mut Buffer,
+    ch: char,
+    n: u8,
+    style: impl Into<Style>,
+) -> Rect {
+    let style = style.into();
+    let char_width = unicode_width::UnicodeWidthChar::width(ch).unwrap_or(0) as u16;
+    let Rect {
+        mut x,
+        y,
+        mut width,
+        ..
+    } = line;
+
+    for _ in 0..n {
+        if width == 0 {
+            break;
+        }
+
+        match buf.cell_mut((x, y)) {
+            Some(cell) => {
+                cell.set_char(ch).set_style(style);
+            }
+            None => break,
+        }
+
+        width = width.saturating_sub(char_width);
+        x += char_width;
+    }
+
+    Rect {
+        x,
+        y,
+        width,
+        height: line.height,
+    }
+}
+
+/// Prints text `n` times.
+pub fn print_text_repeat(
+    line: Rect,
+    buf: &mut Buffer,
+    text: &str,
+    n: u8,
+    style: impl Into<Style>,
+) -> Rect {
+    let style = style.into();
+    let Rect {
+        mut x,
+        y,
+        mut width,
+        ..
+    } = line;
+
+    for _ in 0..n {
+        if width == 0 {
+            break;
+        }
+
+        let (next_x, _) = buf.set_stringn(x, y, text, width as usize, style);
+        width -= next_x - x;
+        x = next_x;
+    }
+
+    Rect {
+        x,
+        y,
+        width,
+        height: line.height,
     }
 }
 
