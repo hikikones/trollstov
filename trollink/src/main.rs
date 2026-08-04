d1be1337aada26b62dc57c2c8deb13a36d51b1f6
hikikones <hikikones@gmail.com>
2026-08-04 22:10:55 +0200

    ensure path exists

--- trollink/src/main.rs
+++ trollink/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use shared::terminal::{Terminal, TerminalInfo};
 use trollink::{app::App, database::Database, settings::Settings};
@@ -6,13 +6,15 @@ use trollink::{app::App, database::Database, settings::Settings};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = clap::Parser::parse();
 
-    #[cfg(debug_assertions)]
-    let database = open_dev_database(args.database)?;
+    let config_dir = get_config_dir();
+
     #[cfg(not(debug_assertions))]
-    let database = open_database(args.database)?;
+    let database = open_database(args.database, config_dir.as_deref())?;
+    #[cfg(debug_assertions)]
+    let database = open_dev_database(args.database, config_dir.as_deref())?;
 
     let term_info = query_term_info()?;
-    let settings = read_settings(args.settings, args.assets, term_info)?;
+    let settings = read_settings(args.settings, args.assets, config_dir.as_deref(), term_info)?;
 
     let mut terminal = Terminal::init()?;
 
@@ -58,76 +60,57 @@ fn query_term_info() -> Result<shared::terminal::TerminalInfo, String> {
         .map_err(|err| format!("Failed to get terminal info: {}", err))
 }
 
-fn open_database(path: Option<PathBuf>) -> Result<Database, String> {
-    fn get_default_database_file() -> Option<PathBuf> {
-        const FILENAME: &str = "database.db";
-        directories::ProjectDirs::from(
-            trollink::APP_QUALIFIER,
-            trollink::APP_ORGANIZATION,
-            trollink::APP_NAME,
-        )
-        .map(|project_dirs| project_dirs.config_dir().join(FILENAME))
-    }
-
-    let file = match path {
-        Some(path) => path,
-        None => match get_default_database_file() {
-            Some(path) => path,
-            None => {
-                return Err(
-                    "Failed to get a default database file path from the operating system",
-                )?;
-            }
-        },
+fn open_database(path: Option<PathBuf>, config_dir: Option<&Path>) -> Result<Database, String> {
+    let Some(file) = path.or(config_dir.map(|dir| dir.join("database.db"))) else {
+        return Err("Failed to get a default database file path from the operating system")?;
     };
 
-    Database::open(&file).map_err(|err| {
-        format!(
-            "Failed to open database \"{}\" due to {}",
-            file.display(),
-            err,
-        )
-    })
+    ensure_path_exists(&file, true)
+        .map_err(|err| format!("Failed to create path for '{}': {}", file.display(), err))?;
+
+    Database::open(&file)
+        .map_err(|err| format!("Failed to open database '{}': {}", file.display(), err))
 }
 
 #[cfg(debug_assertions)]
-fn open_dev_database(path: Option<PathBuf>) -> Result<Database, String> {
+fn open_dev_database(path: Option<PathBuf>, config_dir: Option<&Path>) -> Result<Database, String> {
     if path.is_some() {
-        open_database(path)
+        open_database(path, config_dir)
     } else {
         Database::open_in_memory()
-            .map_err(|err| format!("Failed to open database in memory due to {}", err))
+            .map_err(|err| format!("Failed to open database in memory: {}", err))
     }
 }
 
 fn read_settings(
     config_file: Option<PathBuf>,
     assets_dir: Option<PathBuf>,
+    config_dir: Option<&Path>,
     info: TerminalInfo,
 ) -> Result<Settings, String> {
-    fn get_default_config_file() -> Option<PathBuf> {
-        const FILENAME: &str = "settings.toml";
-        directories::ProjectDirs::from(
-            trollink::APP_QUALIFIER,
-            trollink::APP_ORGANIZATION,
-            trollink::APP_NAME,
-        )
-        .map(|project_dirs| project_dirs.config_dir().join(FILENAME))
-    }
-
-    let config_file = match config_file {
-        Some(path) => path,
-        None => match get_default_config_file() {
-            Some(path) => path,
-            None => {
-                return Err(
-                    "Failed to get a default settings file path from the operating system",
-                )?;
-            }
-        },
+    let Some(config_file) = config_file.or(config_dir.map(|dir| dir.join("settings.toml"))) else {
+        return Err("Failed to get a default settings file path from the operating system")?;
     };
 
-    let assets_dir = get_or_create_assets_dir(assets_dir)?;
+    let Some(assets_dir) = assets_dir.or(config_dir.map(|dir| dir.join("assets"))) else {
+        return Err("Failed to get a default assets directory from the operating system")?;
+    };
+
+    ensure_path_exists(&config_file, true).map_err(|err| {
+        format!(
+            "Failed to create path for '{}': {}",
+            config_file.display(),
+            err
+        )
+    })?;
+
+    ensure_path_exists(&assets_dir, false).map_err(|err| {
+        format!(
+            "Failed to create path for '{}': {}",
+            assets_dir.display(),
+            err
+        )
+    })?;
 
     if !config_file.exists() {
         return Ok(Settings::new(config_file, assets_dir, info));
@@ -136,44 +119,21 @@ fn read_settings(
     Settings::read(config_file, assets_dir, info)
 }
 
-fn get_or_create_assets_dir(path: Option<PathBuf>) -> Result<PathBuf, String> {
-    fn get_default_assets_dir() -> Option<PathBuf> {
-        const DIRNAME: &str = "assets";
-        directories::ProjectDirs::from(
-            trollink::APP_QUALIFIER,
-            trollink::APP_ORGANIZATION,
-            trollink::APP_NAME,
-        )
-        .map(|project_dirs| project_dirs.config_dir().join(DIRNAME))
-    }
-
-    let dir = match path {
-        Some(path) => path,
-        None => match get_default_assets_dir() {
-            Some(path) => path,
-            None => {
-                return Err("Failed to get a default assets directory from the operating system")?;
-            }
-        },
+fn ensure_path_exists(path: &Path, is_file: bool) -> std::io::Result<()> {
+    let dir = if is_file {
+        path.parent().unwrap_or(Path::new("."))
+    } else {
+        path
     };
 
-    // Make sure assets dir is created
-    if !dir.exists() {
-        std::fs::create_dir_all(&dir).map_err(|err| {
-            format!(
-                "Failed to create assets directory at '{}' due to {}",
-                dir.display(),
-                err
-            )
-        })?;
-    }
-    // If already exists, make sure it is actually a dir
-    else if !dir.is_dir() {
-        return Err(format!(
-            "Assets directory '{}' is not a directory",
-            dir.display()
-        ))?;
-    }
+    std::fs::create_dir_all(dir)
+}
 
-    Ok(dir)
+fn get_config_dir() -> Option<PathBuf> {
+    directories::ProjectDirs::from(
+        trollink::APP_QUALIFIER,
+        trollink::APP_ORGANIZATION,
+        trollink::APP_NAME,
+    )
+    .map(|dirs| dirs.config_dir().to_path_buf())
 }
